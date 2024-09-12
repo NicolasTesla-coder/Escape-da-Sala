0c8f0febb6538392d6b583cb238c30b6b6ef9d39
NicolasTesla-coder <00001116686521sp@al.educacao.sp.gov.br>
2024-09-11 22:43:01 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -4,17 +4,30 @@ let gaveta = false;
 
 const avanca = document.querySelectorAll('.btn-proximo');
 const avancaflor = document.querySelectorAll('.btn-flor')
+
 const codigo = document.querySelector('.btn-senha');
 const esquerdainicio = document.querySelector('.btn-proximo-flor')
 const direitainicio = document.querySelector('.btn-proximo-estante')
 
-avanca.forEach(button => {
-    button.addEventListener('click', function(){
+function proximaSala(btn, sala){
+    if (typeof(sala) == 'undefined') {
         const atual = document.querySelector('.ativo');
-        const proximoPasso = 'passo-' + this.getAttribute('data-proximo');
+        const proximoPasso = 'passo-' + btn.getAttribute('data-proximo');
 
         atual.classList.remove('ativo');
         document.getElementById(proximoPasso).classList.add('ativo');
+    } else {
+        const atual = document.querySelector('.ativo');
+        const proximoPasso =  sala;
+
+        atual.classList.remove('ativo');
+        document.getElementById(proximoPasso).classList.add('ativo');
+    }
+}
+
+avanca.forEach(button => {
+    button.addEventListener('click', function(){
+        proximaSala(this)
     })
 })
 
@@ -26,35 +39,19 @@ avancaflor.forEach(button => {
             gaveta = false
         }
         salaflor += 1
-        const atual = document.querySelector('.ativo');
-        const proximoPasso = 'passo-' + this.getAttribute('data-proximo');
-
-        atual.classList.remove('ativo');
-        document.getElementById(proximoPasso).classList.add('ativo');
+        proximaSala(this)
     })
 })
 
 esquerdainicio.addEventListener('click', function(){
-    const atual = document.querySelector('.ativo');
-    const proximoPasso = 'passo-f' + salaflor;
-
-    atual.classList.remove('ativo');
-    document.getElementById(proximoPasso).classList.add('ativo');
+    proximaSala(this, ('passo-f' + salaflor))
 })
 
 direitainicio.addEventListener('click', function(){
     if (gaveta) {
-        const atual = document.querySelector('.ativo');
-        const proximoPasso = 'passo-2a';
-
-        atual.classList.remove('ativo');
-        document.getElementById(proximoPasso).classList.add('ativo');
+        proximaSala(this, 'passo-2a')
     }else {
-        const atual = document.querySelector('.ativo');
-        const proximoPasso = 'passo-2';
-
-        atual.classList.remove('ativo');
-        document.getElementById(proximoPasso).classList.add('ativo');
+        proximaSala(this, 'passo-2')
     }
 })
 
@@ -62,11 +59,7 @@ codigo.addEventListener('click', function(){
     let resultado = prompt('Digite a Senha')
 
     if (resultado == senha){
-        const atual = document.querySelector('.ativo');
-        const proximoPasso = 'passo-' + '1c';
-
-        atual.classList.remove('ativo');
-        document.getElementById(proximoPasso).classList.add('ativo');
+        proximaSala(this, 'passo-1c')
     } else {
         alert('Senha Incorreta')
     }
